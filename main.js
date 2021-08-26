51fe99557c1fa652b759dabcf70254ab8b7ff435
JaimeAndresLopez <james_2005_scott@hotmail.com>
2021-08-26 17:50:09 -0500

    Ejercicio Horario De La Semana Con FormatosDeTexto

---
+++ main.js
@@ -0,0 +1,64 @@
+console.log(`Hola Mundo`);
+
+addEventListener("DOMContentLoaded", (e)=>{
+
+    console.log("DOM cargado...");
+
+
+
+
+
+
+    // Ejemplo 5
+    // let myTag = document.createElement('STYLE');
+    // let myStyle = document.createTextNode(`
+    //     body{
+    //         background-color: red;
+    //         color: white;
+    //     }
+    // `);
+    // myTag.appendChild(myStyle);
+    // document.head.append(myTag);
+
+
+
+    
+
+
+
+    // Ejemplo 4
+    // let id = document.querySelector('body');
+    // id.setAttribute("style", "background-color: green; color: white");
+
+
+
+
+
+    // Ejemplo 3
+    // document.body.style.cssText = `
+    //     background-color: orange;
+    //     color: white;
+    // `;
+
+
+
+
+
+    // Ejemplo 2
+    // let myStyle = {
+    //     "background-color" : "red",
+    //     "color": "white"
+    // }
+    // var {color, ...myStyleUpdate} =  myStyle;
+    // Object.assign(document.body.style, myStyleUpdate);
+    
+
+
+
+
+    // Ejemplo 1
+    // document.body.style.backgroundColor = "red";
+    // document.body.style.color = "white";
+
+
+})
